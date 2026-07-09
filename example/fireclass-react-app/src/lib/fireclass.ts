d05278572c30baf0676fc024022659e1b8e65166
AYUSH DHAR <contact@ayushdhar.com>
2026-07-09 22:41:27 +0530

    chore: remove fireclass react example app files
    delete all files from the fireclass-react example directory to clean up the repository

--- example/fireclass-react-app/src/lib/fireclass.ts
+++
@@ -1,7 +0,0 @@
-import "reflect-metadata";
-import { createFireclass } from "@dharayush7/fireclass-react";
-import { db } from "./firebase";
-
-export const { BaseModel, useQuery, useDoc, adapter } = createFireclass(db);
-
-export { Collection, Subcollection } from "@dharayush7/fireclass-react";
